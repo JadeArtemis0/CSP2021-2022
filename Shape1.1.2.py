d2f2e10f49d72634b8e8a487c7c928778563d8e5
Jade Artemis 0 <89877789+JadeArtemis0@users.noreply.github.com>
2021-09-15 11:20:17 -0700

    Create Shape1.1.2.py

---
+++ Shape1.1.2.py
@@ -0,0 +1,10 @@
+# create turtle object
+painter = trtl.Turtle()
+
+# set the pen and fill colors
+c = input ("What coulor would you like the circle to be?")
+
+pencolor = (c)
+# then draw a circle
+painter.circle(100, 180, 3)
+painter.circle(100, 180, 3)
